80066764f7557c624b1588c7a7ca315d4c7272c8
mariamahmed000 <mariam.memo455@gmail.com>
2023-03-13 20:37:09 +0200

    d

--- Amit_Project/assests/main.js
+++ Amit_Project/assests/main.js
@@ -1,23 +1,4 @@
-// window.onscroll = function() { scrollFunction() };
 
-// function scrollFunction() {
-//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//         document.getElementById("topnav").style.width = "100%";
-//         document.getElementById("header").style.position = "fixed";
-//         document.getElementById("header").style.top = "0%";
-//     } else {
-//         document.getElementById("topnav").style.width = "80%";
-//         document.getElementById("header").style.position = "fixed";
-//         document.getElementById("header").style.top = "2rem";
-//     }
-// }
-
-
-
-
-// window.onscroll = function (e) {
-//     console.log(window.scrollY); // Value of scroll Y in px
-// };
 
 var posts=document.querySelector("#posts")
 var nav=document.querySelector("#header")
